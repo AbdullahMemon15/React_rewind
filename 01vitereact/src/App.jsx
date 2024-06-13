e26c6ee6b0b5fb9d4952063ce5f913af9fb6d779
AbdullahMemon15 <130421052+AbdullahMemon15@users.noreply.github.com>
2024-06-12 21:10:10 -0700

    react flow and structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,14 @@
 // import { useState } from 'react'
-
+import Chai from "./chai"
 function App() {
 
   return (
-    <h1>chai aur react with vite | Abdullah</h1>
+    <>
+    {/* // <h1>chai aur react with vite | Abdullah</h1> */}
+    <Chai/>
+    <h1>dujhgnkjn</h1>
+
+    </>
   )
 }
 
