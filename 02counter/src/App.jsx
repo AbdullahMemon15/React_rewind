8bd66528153da85f53fcc78d1dc6fb19922b51c0
AbdullahMemon15 <130421052+AbdullahMemon15@users.noreply.github.com>
2024-06-13 21:17:53 -0700

    hook project
    project to understand hooks in react

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter = 15;
+
+
+  let [counter,setCounter] = useState(15)
+
+  const addValue = ()=>{
+    if(counter < 20){
+      counter++;
+    setCounter(counter)
+    }
+    else{
+      alert("value can not exceed 20")
+    }
+    // console.log("clicked", counter)
+  }
+
+  const removeValue = ()=>{
+    if(counter > 0){
+      counter--;
+    setCounter(counter)
+    }  else{
+      alert("value can not go below 0")
+    }
+    // console.log("clicked", counter)
+  }
+
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
